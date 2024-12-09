bacdaeed82e4782446780278028bc8e5d916a9a5
Dan O'Meara <danomearawd@gmail.com>
2024-12-09 13:28:38 -0500

    Set up http://localhost:8000/

---
+++ static/js/index.js
@@ -0,0 +1,41 @@
+//IN ORDER FOR THIS CODE TO WORK YOU NEED A
+//LOCAL SERVER. RUN static/py/server.py then use:
+// http://localhost:8000/
+
+
+//initialize geojson file location
+const geojson_file = "../../output/Cleaned_Parking_Violations_DC_09_2024.geojson";
+
+//load the file
+d3.json(geojson_file).then(function (data) {
+    console.log(data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}).catch(function (error) {
+    console.error("Error loading GeoJSON file:", error);
+});
+
+
