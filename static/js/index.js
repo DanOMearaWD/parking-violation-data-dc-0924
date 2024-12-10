00e9a556a59f634a083026e16b5b5e1679eef8e4
Dan O'Meara <danomearawd@gmail.com>
2024-12-09 20:09:51 -0500

    Plotly Test

--- static/js/index.js
+++ static/js/index.js
@@ -86,6 +86,22 @@ d3.json(geojson_file).then(function (data) {
 
 
 
+
+
+
+    TESTER = document.getElementById('tester');
+    Plotly.newPlot(TESTER, [{
+        x: [1, 2, 3, 4, 5],
+        y: [1, 2, 4, 8, 16]
+    }], {
+        margin: { t: 0 }
+    });
+
+
+
+
+
+
 }).catch(function (error) {
     console.error("Error loading GeoJSON file:", error);
 });
